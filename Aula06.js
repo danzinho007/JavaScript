7e3a0e47c29b16ef7762f333fe1aeacfe43f41b6
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-06 18:17:29 -0200

    Create Aula06.js

---
+++ Aula06.js
@@ -0,0 +1,72 @@
+// Comentário em JavaScript 
+/* Comentário em JavaScript */
+// Para executar precisa ter a extensão NodeEjec
+// Apertar F8
+// cálice = acento agurado
+// JavaScript usando crase : Shift + acento depois espaço ``
+
+var d = 1
+while (d < 1000 ) {
+    console.log('Tudo bem ? ' + d)
+    d = d + 100
+}
+var e = 1
+while ( e < 1000000 ) {
+    console.log('Come pizza ' + e)
+    e = e + 10000
+}
+var f = 1
+while ( f < 100000000 ) {
+// 100.000.000
+// 100.000.000.000
+    console.log('Come pizza ' + f)
+    f = f + 1000000
+// 1.000.000
+}
+// Chegou em 6.Trilhão 026.Bilhão 026.Milhão 000.Mil 001
+//          10.        511.       651.       000.    001
+//          13.        921.       611.       000.    001
+var g = 1
+while ( g < 100000000000) {
+    console.log('Come pizza ' + g)
+    g = g + 100000000
+}
+var h = 1
+while ( h < 100000000000000) {
+    console.log('Come pizza ' + h)
+    h = h + 100000000000
+}
+// Chegou a 99.Trilhão 900.Bilhão 000.Milhão 000.Mil 001
+var i = 1
+while ( i < 100000000000000000) {
+    console.log('Come pizza ' + i)
+    i = i + 1000000000000
+}
+// Chegou a 99.QuaTrilhão 999.Trilhão 000.Bilhão 000.Milhão 000.Mil 000
+var j = 1
+while ( j < 10000000000000000000000) {
+//        10 S     000             000            000         000        000        000     000     
+//                 100.Quinquilhão 000.Quatrilhão 000.Trilhão 000.Bilhão 000.Milhão 000.Mil 000
+    console.log('Come pizza ' + j)
+    j = j + 9999999999999999
+// 9999999999999999
+// 9.999.999.999.999.999
+// 100.Trilhão 000.Bilhão 000.Milhão 000.Mil 000
+// 100         000        000        000     000
+}
+//        726.Quad 209.Tri  000.Bi 000.Mi 000.M 000
+// 1.Quin 854.   915.       000.   000.   000.  000
+// 7.     469.   373.       000.   000.   000.  000
+//10.     127.   982.       000.   000.   000.  000
+//17.     021.   758.       000.   000.   000.  000
+//35.     714.   080.       000.   000.   000.  000
+//40.     813.   700.       000.   000.   000.  000
+//293.    683.   499.       976.   061.   420.  000
+//663.    727.   599.       915.   433.   500.  000
+//999.    999.   999.       860.   338.   600.  000
+//1.      949.   590.       998.   472.   283e. +21
+// é igual a : 
+//13.817.713.000.614.794 
+//Lv 300MS :2,   058,       731,   433,   259,  209
+// https://strategywiki.org/wiki/MapleStory/EXP_and_Pet_Closeness
+// http://www.southperry.net/showthread.php?t=82868
