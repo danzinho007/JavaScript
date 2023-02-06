f7d4976e17ee80a1427eb10e504cd2470a768db0
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-06 18:17:27 -0200

    Create Aula05.js

---
+++ Aula05.js
@@ -0,0 +1,31 @@
+// Comentário em JavaScript 
+/* Comentário em JavaScript */
+// Para executar precisa ter a extensão NodeEjec
+// Apertar F8
+// cálice = acento agurado
+// JavaScript usando crase : Shift + acento depois espaço ``
+
+var c = 1
+while ( c < 6 ) {
+    console.log('Tudo bem ? ' + c)
+    c++
+}
+// c++ = c = c + 1
+// Faz 5x pois c começou com 1 
+// ou
+// Porque c é menor 6
+
+var d = 1
+while (d <= 6 ) {
+    console.log('Tudo bem ? ' + d)
+    d++
+}
+
+var e = 1
+while ( e < 7 ) {
+    console.log('Come pizza ' + e)
+    e = e + 1
+}
+
+// Aqui fez 6x, pois começou com 1
+
