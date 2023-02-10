e4b1543b410937425cae17f24ffc50a83bc54fac
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-10 01:15:48 -0200

    Create Aula00 copy 4.js

---
+++ Aula00 copy 4.js
@@ -0,0 +1,7 @@
+// Comentário em JavaScript 
+/* Comentário em JavaScript */
+// Para executar precisa ter a extensão NodeEjec
+// Apertar F8
+// cálice = acento agurado
+// JavaScript usando crase : Shift + acento depois espaço ``
+
