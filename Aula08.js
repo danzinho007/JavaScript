12896ca76ab0e57266c2fd6be2298ccca58b0d9b
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-10 01:16:01 -0200

    Create Aula08.js

---
+++ Aula08.js
@@ -0,0 +1,32 @@
+// Comentário em JavaScript 
+/* Comentário em JavaScript */
+// Para executar precisa ter a extensão NodeEjec
+// Apertar F8
+// cálice = acento agurado
+// JavaScript usando crase : Shift + acento depois espaço ``
+
+// Contar até 5 :
+console.log(`Código B1`)
+console.log(`Código B2`)
+console.log(`Código B3`)
+console.log(`Código B4`)
+console.log(`Código B5`)
+
+// Contar até 5 usando while :
+var c = 1
+while ( c <= 5 ) {
+    console.log(`Código C`+ c )
+    c = c + 1 // ou c++
+}
+
+// Contar até 5 usando do ... while :
+var d = 1
+do {
+    console.log(`Código D` + d)
+    d++
+} while ( d <= 5)
+
+// Contar até 5 usando for :
+for ( var e = 1; e <= 5; e++) {
+    console.log(`Código E` + e)
+}
