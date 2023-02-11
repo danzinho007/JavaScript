008a0f0eae3eafbed756602e675ae2e1b8e43738
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-11 12:48:26 -0200

    Update Aula09.js

--- Aula09.js
+++ Aula09.js
@@ -5,3 +5,29 @@
 // cálice = acento agurado
 // JavaScript usando crase : Shift + acento depois espaço ``
 
+// Funções Básicas 
+// https://learnjavascript.online/app.html
+
+function soma(x, y) {
+    return x + y; // A palavra-chave return encerra função com o resultado (que é x + y), portanto, o código posterior nunca será executado!
+    console.log("Hello World");
+}
+
+let resultado1 = soma(1, 2); // let é uma palavra-chave em JavaScript para guardar variáveis
+console.log(resultado1); // Mostra na tela o que está guardado na variável resultado
+// Resultado 3
+
+var resultado2 = soma(3, 4); // var também é uma palavra-chave em JavaScript para guardar variáveis
+console.log(resultado2);
+// Resultado 7
+
+console.log(soma(5, 6)); // Forma resumida
+// Resultado 11
+
+function multi(x, y) {
+    return x * y
+}
+
+console.log(multi(2, 2));
+// Resultado 4
+
