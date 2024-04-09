3aa65a704b981fc21d2819b5e87ddb3124176d47
danzinho008 <55755085+danzinho007@users.noreply.github.com>
2024-04-08 21:21:14 -0300

    mod

--- 01ate30/Aula01.js
+++ 01ate30/Aula01.js
@@ -7,12 +7,24 @@ var vel = 30
 console.log('A velocidade do seu carro é de ${vel}Km/h')
 // Aspas simples não funciona, precisa ser crase ( shift + acento )
 // se usar acento agudo ( ex: cálice ) vai dar erro
+
+// Uando crase temos a interpolação :
 console.log(`A velocidade do seu carro é de ${vel}Km/h`)
 if (vel > 60) {
     console.log('Você ultrapassou a velocidade permitida, MULTADO!')
 }
 console.log('Dirija sempre usando cinto de segurança')
 console.log(' ')
+
+// Usando velocidade acima de 60 :
+var vel1 = 70
+console.log(`A velocidade do seu carro é de ${vel1}Km/h`)
+if (vel1 > 60) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
+console.log(' ')
+
 var país1 = 'Brasil'
 console.log(`Vivendo na/no ${país1}`)
 if (país1 == 'Brasil') {
@@ -20,8 +32,7 @@ if (país1 == 'Brasil') {
 } else {
     console.log('Você é Estrangeiro')
 }
-console.log(' ')
-console.log('Dirija sempre usando cinto de segurança')
+
 console.log(' ')
 var país2 = 'França'
 console.log(`Vivendo na/no ${país2}`)
