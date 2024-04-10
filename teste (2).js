ede6a0d566e99b2326b9a85846127475b82ad4de
danzinho008 <55755085+danzinho007@users.noreply.github.com>
2024-04-10 03:02:15 -0300

    add

---
+++ teste (2).js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verificação da Versão do ECMAScript</title>
+    <script src="check-es-version.js"></script>
+</head>
+<body>
+    <!-- Conteúdo da sua página -->
+</body>
+</html>
